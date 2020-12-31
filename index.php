5fa821fc3d5cdaf6bcbf214fd31a0e4833b3b1c2
MehediHasanEmon21 <mh.emon2021@gmail.com>
2020-12-31 14:38:36 -0800

    inheritence exm

--- index.php
+++ index.php
@@ -1,43 +1,28 @@
 <?php
 
-class Employee{
+class Base{
 
-	public $name,$age,$salary;
+	public $name = "A";
 
-	public function __construct($n,$a,$s){
-		$this->name = $n;
-		$this->age = $a;
-		$this->salary = $s;
-	}
+	public function calc($a,$b){
 
-	public function info(){
-		echo "<h2>Employee Profile</h2>";
-		echo "<h4>Name :".$this->name."</h4>";
-		echo "<h4>Age :".$this->age."</h4>";
-		echo "<h4>Salary :".$this->salary."</h4>";
+		echo $a+$b;
+
 	}
 
 }
 
-class Manager extends Employee{
+class Derived extends Base{
+	public $name = "B";
 
-	public $ta = 2000;
-	public $phone = 1000;
-	public $totalSalary;
+	public function calc($a,$b){
 
-	public function info(){
-		$this->totalSalary = $this->salary + $this->ta + $this->phone;
-		echo "<h2>Manager Profile</h2>";
-		echo "<h4>Name :".$this->name."</h4>";
-		echo "<h4>Age :".$this->age."</h4>";
-		echo "<h4>Salary :".$this->totalSalary."</h4>";
+		echo $a*$b;
+
 	}
-
-
 }
 
-$e1 = new Employee("Emon",20,2000);
-$e2 = new Manager("Hasan",22,3000);
-$e1->info();
-$e2->info();
+$d = new Derived();
+echo $d->name."<br>";
+$d->calc(10,5);
 
